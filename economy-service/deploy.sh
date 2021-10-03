71b0b485a8fb1a9fe28629c2eb9726511a840fbf
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-10-03 15:52:00 +0300

    deploy updates

--- economy-service/deploy.sh
+++ economy-service/deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:DanTrofimov/economyService-FE.git main:gh-pages
+git push -f git@github.com:DanTrofimov/economyService-FE.git master:gh-pages
 
 cd -
