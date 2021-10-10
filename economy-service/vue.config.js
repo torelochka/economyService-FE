f05c8b34f0f363599becf7c8fd2e0d800a45d471
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-10-10 04:13:12 +0300

    update favicon

--- economy-service/vue.config.js
+++ economy-service/vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/economyService-FE/'
-        : '/'
-}
+        : '/',
+    pwa: {
+        name: "m8.checker",
+        themeColor: 'purple',
+        msTileColor: 'purple',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'purple',
+
+        manifestOptions: {
+            name: "m8.checker",
+            short_name: "m8.checker",
+            theme_color: "purple",
+        },
+    }
+};
+
