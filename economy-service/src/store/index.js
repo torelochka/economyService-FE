1f2f954421ce2de6829bca6d8af4209f551075ff
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-10-12 01:12:28 +0300

    endpoint fixes

--- economy-service/src/store/index.js
+++ economy-service/src/store/index.js
@@ -11,17 +11,16 @@ export default new Vuex.Store({
         async sendGroupData(ctx, { groups, photos }) {
             let formData = new FormData();
 
-            formData.append("groupNumbers", [groups]);
+            formData.append("groupNumbers", groups);
             formData.append("studentsImages", photos);
-            console.log(process.env.VUE_APP_CORS_URL);
+            console.log(groups);
+            console.log(photos);
 
-            let response = await fetch(`${process.env.VUE_APP_BASE_URL}check`, {
+            let response = await fetch(`${process.env.VUE_APP_CORS_URL}${process.env.VUE_APP_BASE_URL}check`, {
                 method: "POST",
-                mode: "no-cors",
                 body: formData,
                 headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "*/*",
+                    'X-Requested-With': 'XMLHttpRequest'
                 }
             });
             let result = await response;
