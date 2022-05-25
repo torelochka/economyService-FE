3c32397de5a9820beae03a02b4a52f4ba14f513a
torelochka <zheleznov213@gmail.com>
2022-05-25 21:19:01 +0300

    deploy

---
+++ precache-manifest.4d3e45b324dba2d8bcfc0d7caaba3e16.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4946ed9ab008ca1b8b4",
+    "url": "/economyService-FE/css/app.eafe60aa.css"
+  },
+  {
+    "revision": "5a63a947cdfb7da71aab",
+    "url": "/economyService-FE/css/chunk-0d8e99a8.8afcbbb1.css"
+  },
+  {
+    "revision": "b1f939493c186cb6d48b",
+    "url": "/economyService-FE/css/chunk-106a47bc.a79e31c0.css"
+  },
+  {
+    "revision": "4161b6eaa95493ebb72f",
+    "url": "/economyService-FE/css/chunk-4736dead.0ccd1671.css"
+  },
+  {
+    "revision": "33941831dda07be9e045",
+    "url": "/economyService-FE/css/chunk-9ae89b06.77b6d4c4.css"
+  },
+  {
+    "revision": "dd09153977944b7478ca",
+    "url": "/economyService-FE/css/chunk-f5691526.7d7eed43.css"
+  },
+  {
+    "revision": "53ce8c8840322a10f92c5828d3221e22",
+    "url": "/economyService-FE/fonts/Consolas.53ce8c88.ttf"
+  },
+  {
+    "revision": "53ce8c8840322a10f92c5828d3221e22",
+    "url": "/economyService-FE/fonts/Consolas.ttf"
+  },
+  {
+    "revision": "2a42807ab042a0345f27581b1f5d8cd8",
+    "url": "/economyService-FE/img/eye.2a42807a.gif"
+  },
+  {
+    "revision": "0ebd759eab5941ce8f1bb0f2d8e96920",
+    "url": "/economyService-FE/index.html"
+  },
+  {
+    "revision": "e4946ed9ab008ca1b8b4",
+    "url": "/economyService-FE/js/app.739f3d7a.js"
+  },
+  {
+    "revision": "5a63a947cdfb7da71aab",
+    "url": "/economyService-FE/js/chunk-0d8e99a8.bc285df2.js"
+  },
+  {
+    "revision": "b1f939493c186cb6d48b",
+    "url": "/economyService-FE/js/chunk-106a47bc.7000ec49.js"
+  },
+  {
+    "revision": "4161b6eaa95493ebb72f",
+    "url": "/economyService-FE/js/chunk-4736dead.d9c05355.js"
+  },
+  {
+    "revision": "33941831dda07be9e045",
+    "url": "/economyService-FE/js/chunk-9ae89b06.e75a616d.js"
+  },
+  {
+    "revision": "dd09153977944b7478ca",
+    "url": "/economyService-FE/js/chunk-f5691526.c1166896.js"
+  },
+  {
+    "revision": "2f2093149ce26b19b5e0",
+    "url": "/economyService-FE/js/chunk-vendors.4c42221e.js"
+  },
+  {
+    "revision": "2b20950039ef0cf57318fdf080757341",
+    "url": "/economyService-FE/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/economyService-FE/robots.txt"
+  }
+]);
